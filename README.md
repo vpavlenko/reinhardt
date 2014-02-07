03f46021f9efae63505aedfb6308f373feea7df0
vpavlenko <cxielamiko@gmail.com>
2014-02-07 14:58:12 +0400

    show directories and files inside subdirectories

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ v0.1: manage static files in a directory
 Test:
 
     ./server.py 8095 projects/hello_kitty/
+
+v0.2: list files in a directory
+Test:
+
+    ./server.py 8095 projects/dir_with_files/
--- server.py
+++ server.py
@@ -5,11 +5,14 @@ import os
 import re
 import socket
 import sys
+import urllib.parse
 
-VERSION = 0.1
+
+VERSION = 0.2
 HOST = '127.0.0.1'
 MAX_CONNECTIONS = 100  # number of simultaneous connections waiting in a queue
 BUFLEN = 1024  # read data from socket by chunks of size BUFLEN
+FILESYSTEM_ENCODING = sys.getfilesystemencoding()  # encoding for file names
 
 
 class Request:
@@ -20,7 +23,7 @@ class Request:
     def __init__(self, data):
         first_line, *headers_lines = re.split('\r?\n', data)
         self.method, self.path, self.http_version = first_line.split(' ')
-        self.path = self.path[1:]
+        self.path = urllib.parse.unquote(self.path[1:], encoding=FILESYSTEM_ENCODING)
         self.headers = {}
         for line in headers_lines:
             if line:
@@ -40,11 +43,11 @@ def read_data_from_socket(sock):
 
 def compile_response(code, comment, mimetype, body):
     assert type(body) == bytes
-    head = '''HTTP/1.0 {0} {1}\r
-Content-Type: {2}\r
-Content-Length: {3}\r
-\r
-'''.format(code, comment, mimetype, len(body)).encode('ascii')
+    head = ('HTTP/1.0 {0} {1}\r\n'
+            'Content-Type: {2}\r\n'
+            'Content-Length: {3}\r\n\r\n').format(
+                code, comment, mimetype, len(body)
+            ).encode('ascii')
     return head, body
 
 
@@ -54,6 +57,32 @@ def http_404(request, message):
                             mimetype='text/html', body=body.encode('ascii'))
 
 
+def send_file(request, path):
+    data = open(path, 'rb').read()
+    mimetype = mimetypes.guess_type(path)[0]
+    if mimetype is None:
+        mimetype = 'octet/stream'  # default action: propose to download
+    return compile_response(code=200, comment='OK', mimetype=mimetype,
+                            body=data)
+
+
+def send_directory(request, path):
+    # Items are sorted in the following order: all directories, then all files.
+    # Alphabetical order inside each group.
+    files = [(not os.path.isdir(os.path.join(path, filename)), filename)
+             for filename in os.listdir(path)]
+    files.sort()
+    files = [filename + ('' if isfile else '/') for isfile, filename in files]
+
+    encoding_line = '<meta charset="{encoding}">'
+    file_line = '<div><a href="{filename}">{filename}</a></div>'
+    data = ([encoding_line.format(encoding=FILESYSTEM_ENCODING)] +
+            [file_line.format(path=path, filename=filename)
+             for filename in files])
+    return compile_response(code=200, comment='OK', mimetype='text/html',
+                            body='\n'.join(data).encode(FILESYSTEM_ENCODING))
+
+
 def serve_static(request, directory):
     path = os.path.join(directory, request.path)
     if not os.path.exists(path):
@@ -61,13 +90,13 @@ def serve_static(request, directory):
                         message='Path {0} doesn\'t exist'.format(path))
     else:
         if os.path.isdir(path):
-            path = os.path.join(path, 'index.html')
-        data = open(path, 'rb').read()
-        mimetype = mimetypes.guess_type(path)[0]
-        if mimetype is None:
-            mimetype = 'octet/stream'  # default action: propose to download
-        return compile_response(code=200, comment='OK', mimetype=mimetype,
-                                body=data)
+            index = os.path.join(path, 'index.html')
+            if os.path.exists(index):
+                return send_file(request, index)
+            else:
+                return send_directory(request, path)
+        else:
+            return send_file(request, path)
 
 
 def static_server(port, directory):
